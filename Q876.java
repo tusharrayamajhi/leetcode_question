3da4f314f4442f21891a4f4c9f8361de5d395123
Tushar Rayamajhi <84193854+tusharrayamajhi@users.noreply.github.com>
2023-02-21 12:34:36 +0545

    Update Q876.java

--- Q876.java
+++ Q876.java
@@ -1,37 +1,23 @@
-import java.util.PrimitiveIterator.OfDouble;
-
+import java.util.*;;
 public class Q876 {
-
-    public class ListNode {
+    class ListNode{
         int data;
         ListNode next;
-
-        ListNode(int data) {
-            this.data = data;
-            this.next = null;
+       public  ListNode(int data){
+        this.data = data;
+        this.next = null;
         }
     }
-
     public static ListNode head;
-    public static  ListNode tail;
+    public static ListNode tail;
 
-    public  void add(int data){
-        ListNode newNode = new ListNode(data);
-        if(head == null){
-            head = tail = newNode;
-            return;
-        }
-        newNode.next = head;
-        head = newNode;
+    //only down part of code is need in leetcode
 
-
-    }
-    
-    public  static ListNode mid(){
+    public static ListNode mid() {
         ListNode fast = head;
         ListNode slow = head;
-        while(fast != null){
-            if(fast.next != null){
+        while (fast != null) {
+            if (fast.next != null) {
                 break;
             }
             fast = fast.next.next;
@@ -40,23 +26,5 @@ public class Q876 {
         return slow;
 
     }
-    public void print(ListNode mid){
-        ListNode temp = mid;
-        while(temp != null){
-            System.out.println(temp.data);
-            temp = temp.next;
-        }
-    }
-    public static void main(String[] args) {
-        Q876 ll = new Q876();
-        ll.add(5);
-        ll.add(4);
-        ll.add(3);
-        ll.add(2);
-        ll.add(1);
-        ListNode mid = mid();
-        ll.print(mid);
-       // System.out.println(ll.mid());
 
-    }
 }
